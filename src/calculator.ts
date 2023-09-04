60778ddd471940f48ee3f91cd6c8a24f305fd2db
Meryem İris <irismeryem.96@gmail.com>
2023-09-04 12:37:11 +0300

    delete calculator.ts

--- src/calculator.ts
+++
@@ -1,8 +0,0 @@
-export const calculate = (expression: string): string => {
-  try {
-    const result = eval(expression);
-    return result.toString();
-  } catch (error) {
-    return "Error";
-  }
-};
