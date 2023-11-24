8432b48e08664ff177c911d091b69e42857e8ff6
Meryem İris <irismeryem.96@gmail.com>
2023-11-25 01:41:41 +0300

    add settingsMenu

--- src/component/Settings.tsx
+++ src/component/Settings.tsx
@@ -1,11 +1,38 @@
 import { FaCog } from "react-icons/fa";
 import styles from "./Settings.module.css";
+// import { useState } from "react";
+
 function Settings() {
+  // const [isHistory, setIsHistory] = useState(false);
+
+  // function toggleHistory() {
+  //   setIsHistory(!isHistory);
+  // }
+
+  function SettingsMenu() {
+    return (
+      <div className={styles.dropdownContent}>
+        <ul>
+          <li>
+            <p>Show History</p>
+            <label className={styles.switch}>
+              <input name="toggleHistory" type="checkbox" />
+              <span className={`${styles.slider} ${styles.round}`}></span>
+            </label>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.settings}>
-      <button className={styles.settingsIcon}>
-        <FaCog />
-      </button>
+      <div className={styles.dropdown}>
+        <button className={styles.settingsButton}>
+          <FaCog />
+        </button>
+        <SettingsMenu />
+      </div>
     </div>
   );
 }
